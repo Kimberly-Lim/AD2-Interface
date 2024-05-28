23cf56b330b69a9363f29446db93b99a940028ec
kimberlylim <knlim@crimson.ua.edu>
2024-05-28 10:59:23 +0200

    Updates start and stop frequency to dropdown menus

--- AnalogImpedance_Working_Ver.py
+++ AnalogImpedance_Working_Ver.py
@@ -1,5 +1,6 @@
 
 
+
 """
    DWF Python Example
    Author:  Digilent, Inc.
@@ -24,7 +25,7 @@ import os
 import csv 
 import threading
 
-progressCounter = 0
+
 steps = 151
 startFrequency = 1e2
 stopFrequency = 1e6
@@ -108,16 +109,13 @@ def makeMeasurement(steps, startFrequency, stopFrequency, reference, voltage, ma
     rgIc = [0.0]*steps # imag current
 
     for i in range(steps):
-        for step in range(steps):
-            hz = stopFrequency * pow(10.0, 1.0*(1.0*i/(steps-1)-1)*math.log10(stopFrequency/startFrequency)) # exponential frequency steps
-            print("Step: "+str(i)+" "+str(hz)+"Hz")
-            rgHz[i] = hz
-            dwf.FDwfAnalogImpedanceFrequencySet(hdwf, c_double(hz)) # frequency in Hertz
+        hz = stopFrequency * pow(10.0, 1.0*(1.0*i/(steps-1)-1)*math.log10(stopFrequency/startFrequency)) # exponential frequency steps
+        print("Step: "+str(i)+" "+str(hz)+"Hz")
+        rgHz[i] = hz
+        dwf.FDwfAnalogImpedanceFrequencySet(hdwf, c_double(hz)) # frequency in Hertz
         # if settle time is required for the DUT, wait and restart the acquisition
         # time.sleep(0.01) 
         # dwf.FDwfAnalogInConfigure(hdwf, c_int(1), c_int(1))
-            updateProgress(step, steps)
-
         while True:
             if dwf.FDwfAnalogImpedanceStatus(hdwf, byref(sts)) == 0:
                 dwf.FDwfGetLastErrorMsg(szerr)
@@ -194,36 +192,44 @@ def update_steps(*args):
     try:
         # Convert the entry to an integer
         steps_int = int(steps.get())
+        if steps_int < 0:
+            raise ValueError("Steps cannot be negative.")
         print("Updated Steps to:", steps_int)
 
-    except ValueError:
-        print("Invalid input for steps. Please enter an integer.")
-        steps.set("151")  # Reset to default if invalid
+    except ValueError as e:
+        print("Invalid input for steps. Please enter an integer")
 
-#Extracts Start value from GUI
-def update_start(*args):
-    global start_int
-    try:
+   # Function to update start frequency
+def update_start_frequency(*args):
+    global startFrequency 
+    startFrequency = startF_var.get()
+    startFrequency_values = {
+        "1 Hz" : 1,
+        "10 Hz" : 10,
+        "100 Hz" : 100,
+        "1000 Hz" : 1000,
+        "10000 Hz" : 10000,
+        "100000 Hz" : 100000,
+        "1 MHz" : 1000000,
+        "10 MHz" : 10000000,
+        "15 MHz" : 15000000
+    }   
 
-        # Convert the entry to an integer
-        start_int = int(startF.get())
-        print("Updated Start to:", start_int)
-
-    except ValueError:
-        print("Invalid input for start. Please enter an integer.")
-        startF.set("100")  # Reset to default if invalid
-
-#Extracts Stop value from GUI
-def update_stop(*args):
-    global stop_int
-    try:
-        # Convert the entry to an integer
-        stop_int = int(stopF.get())
-        print("Updated Stop to:", stop_int)
-
-    except ValueError:
-        print("Invalid input for start. Please enter an integer.")
-        stopF.set("15000000")  # Reset to default if invalid
+   # Function to update stop frequency
+def update_stop_frequency(*args):
+    global stopFrequency 
+    stopFrequency = stopF_var.get()
+    stopFrequency_values = {
+        "1 Hz" : 1,
+        "10 Hz" : 10,
+        "100 Hz" : 100,
+        "1000 Hz" : 1000,
+        "10000 Hz" : 10000,
+        "100000 Hz" : 100000,
+        "1 MHz" : 1000000,
+        "10 MHz" : 10000000,
+        "15 MHz" : 15000000
+    }     
 
 # Function to update amplitude
 def update_amplitude(*args):
@@ -259,8 +265,8 @@ def update_resistance(*args):
 def measure():
     # Update global variables with the selected values
     steps = int(steps_entry.get())
-    start = float(start_freq_entry.get())
-    stopFrequency = float(stop_freq_entry.get())
+    startFrequency = float(startF_var.get().split()[0])
+    stopFrequency = float(stopF_var.get().split()[0])
     reference = float(resistance_var.get().split()[0])
     amplitude = float(amplitude_var.get().split()[0])
     measure_interval = float(measure_interval_entry.get())
@@ -272,20 +278,9 @@ def measure():
     pb['value'] = 0
     value_label['text'] = updateProgressLabel()
 
-    # def update_plot(frequencies, resistances):
-    #     plt.figure()
-    #     plt.plot(frequencies, resistances)
-    #     plt.xscale('log')
-    #     plt.yscale('log')
-    #     plt.xlabel('Frequency (Hz)')
-    #     plt.ylabel('Resistance (Ohms)')
-    #     plt.title('Frequency vs Resistance')
-    #     plt.show()
-
 # Function to handle Start and Stop
 def staart():
     print("Measurement started")
-
 
 def stoop():
     print("Measurement stopped")
@@ -298,18 +293,18 @@ steps_entry = ttk.Entry(root, textvariable=steps)
 steps_entry.grid(row=1, column=0)
 
 # Start Frequency entry
-tk.Label(root, text="Start Freq(Hz)").grid(row=0, column=1)
-startF = tk.StringVar(value="100")  # Default value
-startF.trace_add("write", update_start)  # Trace changes
-start_freq_entry = ttk.Entry(root, textvariable=startF)
-start_freq_entry.grid(row=1, column=1)
+tk.Label(root, text="Start Frequency").grid(row=0, column=1)
+startF_var = tk.StringVar()
+startF_dropdown = ttk.Combobox(root, textvariable=startF_var, values = ["1 Hz", "10 Hz", "100 Hz", "1000 Hz", "10000 Hz", "100000 Hz", "1 MHz", "10 MHz", "15 MHz"])
+startF_dropdown.grid(row=1, column=1)
+startF_dropdown.current(1) # default selection
 
-# Stop Frequency entry
-tk.Label(root, text="Stop Freq(Hz)").grid(row=0, column=2)
-stopF = tk.StringVar(value="15000000")  # Default value
-stopF.trace_add("write", update_stop)  # Trace changes
-stop_freq_entry = ttk.Entry(root, textvariable=stopF)
-stop_freq_entry.grid(row=1, column=2)
+# Stop Frequency entry dropdown
+tk.Label(root, text="Stop Frequency").grid(row=0, column=2)
+stopF_var = tk.StringVar()
+stopF_dropdown = ttk.Combobox(root, textvariable=stopF_var, values = ["1 Hz", "10 Hz", "100 Hz", "1000 Hz", "10000 Hz", "100000 Hz", "1 MHz", "10 MHz", "15 MHz"])
+stopF_dropdown.grid(row=1, column=2)
+stopF_dropdown.current(1) # default selection
 
 
 # Amplitude dropdown
@@ -336,18 +331,12 @@ measure_interval_entry.insert(0, "4")  # Default value
 def updateProgressLabel():
     return f"Current Progress: {pb['value']}%"
 
-# def progress():
-#     if pb['value'] < 100:
-#         pb['value'] += 20
-#         value_label['text'] = updateProgressLabel()
-#     # else:
-    #     # showinfo(message='The progress completed!')
-
-# Function to update the progress bar value
-def updateProgress(current_step, total_steps):
+def progress():
     if pb['value'] < 100:
-        pb['value'] += (current_step / total_steps) * 100
+        pb['value'] += 20
         value_label['text'] = updateProgressLabel()
+    # else:
+    #     # showinfo(message='The progress completed!')
 
 def stop():
     pb.stop()
@@ -368,13 +357,17 @@ value_label = ttk.Label(root, text=updateProgressLabel())
 value_label.grid(column=0, row=7, columnspan=2)
 
 # start button
-start_button = ttk.Button(root, text='Start', command=lambda: threading.Thread(target=makeMeasurement, args=(steps, startFrequency, stopFrequency, reference, 1, 6)).start())
+start_button = ttk.Button(
+    root,
+    text='Start',
+    command=measure
+)
 start_button.grid(column=0, row=6, padx=10, pady=10, sticky=tk.E)
 
 stop_button = ttk.Button(
     root,
     text='Stop',
-    command=stop
+    command=stoop
 )
 stop_button.grid(column=1, row=6, padx=10, pady=10, sticky=tk.W)
 
